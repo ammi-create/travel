19b860041fac5835aa6d4d8f0d5da07348945ed3
ammi <marine.mammal.071@gmail.com>
2024-01-29 01:14:06 +0900

    作成

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(function () {
+  $('.menu-trigger').click(function () {
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+      $('.g-navi').addClass('active');
+    } else {
+      $('.g-navi').removeClass('active');
+    }
+  });
+  $('.nav-wrapper ul li a').click(function () {
+    $('.menu-trigger').removeClass('active');
+    $('.g-navi').removeClass('active');
+  });
+});
+
+$(window).on('load resize', function () {
+  var winW = $(window).width();
+  var devW = 768;
+  if (winW <= devW) {
+    $(function () {
+      $('#main-visual').bgSwitcher({
+        images: ['./image/sp/hill.png', './image/sp/mt.png', './image/sp/shrine.png', './image/sp/aqua.png'],
+        interval: 3000,
+        loop: true,
+        effect: 'fade'
+      });
+    });
+  } else {
+    $(function () {
+      $('#main-visual').bgSwitcher({
+        images: ['./image/hill2.png', './image/mt2.png', './image/shrine2.png', './image/aqua2.png'],
+        interval: 3000,
+        loop: true,
+        effect: 'fade'
+      });
+    });
+  }
+});
+
+$(function () {
+  $('.modalopen').each(function () {
+    $(this).on('click', function () {
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      console.log(modal);
+      $(modal).fadeIn();
+      return false;
+    })
+  })
+  $('.modalClose').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  })
+})
